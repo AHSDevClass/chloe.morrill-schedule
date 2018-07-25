bf209757eb728f58d6ce4a2fb397cd96902e31a3
Chloe Morrill <chloemorrill@Chloes-MacBook-Pro.local>
2018-07-25 08:27:04 -0600

    mapped course object data into library course componenet instances

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -8,14 +8,16 @@ class LibraryCourse extends Component {
         return (
             <div className="library-course">
                 <div className="library-course__title-check">
-                    <label className="library-course__title">Problem Solving</label>
+                    <label className="library-course__title">{ this.props.title }</label>
                     {Icon('fas fa-check', 'library-course__icon')}
                 </div>
                 <Arrow className="library-course__arrow" />
                 <Action className="library-course__action"/>
                 <div className="library-course__description">
                     <label>Course Description</label>
-                    <p>Dummy text. Lots and lots of dummy text so that I can fill the space. I'm really excited for this dummy text to be read. Are you going to read it all or are you going to be lazy and skip it all? What? Just wondering. Also, what's your favorite Netflix show?</p>
+                    <p>
+                        { this.props.description }
+                    </p>
                 </div>
             </div>
         )
